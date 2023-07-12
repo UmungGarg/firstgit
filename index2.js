10362a38c95759fef191af006b6aefd66820f0d8
UmungGarg <handsome.anmol5>
2023-07-12 18:27:19 +0530

    addli

--- index2.js
+++ index2.js
@@ -1,6 +1,17 @@
-var itoms = document.getElementsByClassName('list-group-item');
-console.log(itoms);
-itoms[2].style.backgroundColor= 'green';
-for(var i=0; i<itoms.length; i++){
-    itoms[i].style.fontWeight='bold';
-}
+//var itoms = document.getElementsByClassName('list-group-item');
+//console.log(itoms);
+//itoms[2].style.backgroundColor= 'green';
+//for(var i=0; i<itoms.length; i++){
+ //   itoms[i].style.fontWeight='bold';
+//}
+var items= document.getElementsByClassName('list-group-item');
+console.log(items);
+items[2].style.backgroundColor= 'green';
+for(var i=0; i<items.length; i++){
+       items[i].style.fontWeight='bold';}
+
+       var itms= document.getElementsByTagName('li');
+       console.log(itms);
+       itms[2].style.backgroundColor= 'green';
+       for(var i=0; i<itms.length; i++){
+              itms[i].style.fontWeight='bold';}
